624d4779c99463f4104006b413f0380b24bea44b
shailgitub <shailendra.itpro@gmail.com>
2025-06-10 21:28:45 +0530

    commit links

--- headerFooterFraments.js
+++ headerFooterFraments.js
@@ -129,36 +129,21 @@ class SpecialFooter extends HTMLElement {
 
           <div class="col-lg-3 col-md-6">
             <h4 class="text-light mb-4">Services</h4>
-            <a class="btn btn-link" href="">Air Freight</a>
-            <a class="btn btn-link" href="">Sea Freight</a>
-            <a class="btn btn-link" href="">Road Freight</a>
-            <a class="btn btn-link" href="">Logistic Solutions</a>
-            <a class="btn btn-link" href="">Industry solutions</a>
+            <a class="btn btn-link" href="servicehouseshift.html">House Shifting</a>
+            <a class="btn btn-link" href="serviceofficeshift.html">Office Reloaction</a>
+            <a class="btn btn-link" href="servicecitytrans.html">InterCity Transport</a>
+            <a class="btn btn-link" href="servicegoodstrans.html">Goods & Cargo</a>
+            <a class="btn btn-link" href="servicevehicleshift.html">Vehicle Shifting</a>
           </div>
           <div class="col-lg-3 col-md-6">
             <h4 class="text-light mb-4">Quick Links</h4>
-            <a class="btn btn-link" href="">About Us</a>
-            <a class="btn btn-link" href="">Contact Us</a>
-            <a class="btn btn-link" href="">Our Services</a>
+            <a class="btn btn-link" href="index.html#about">About Us</a>
+            <a class="btn btn-link" href="contact.html">Contact Us</a>
+            <a class="btn btn-link" href="index.html#services">Our Services</a>
             <a class="btn btn-link" href="">Terms & Condition</a>
             <a class="btn btn-link" href="">Support</a>
           </div>
-          <div class="col-lg-3 col-md-6">
-            <h4 class="text-light mb-4">Newsletter</h4>
-            <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
-            <div class="position-relative mx-auto" style="max-width: 400px">
-              <input
-                class="form-control border-0 w-100 py-3 ps-4 pe-5"
-                type="text"
-                placeholder="Your email"
-              />
-              <button
-                type="button"
-                class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2"
-              >
-                SignUp
-              </button>
-            </div>
+          
           </div>
         </div>
       </div>
