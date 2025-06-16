7bf75d2f18463a54ba31f64f21bd800d9aed6e78
shailgitub <shailendra.itpro@gmail.com>
2025-06-17 01:54:27 +0530

    m

--- img/forms/mail.php
+++ img/forms/mail.php
@@ -1,47 +1,38 @@
 <?php
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    header("Location: https://javatechrunner.online/index.html", true, 303);
+    exit;
+}
 
+// Sanitize input
 $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
-
 $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-//$contact = filter_var($_POST['contact'], FILTER_SANITIZE_NUMBER_INT);
 $subject = filter_var($_POST['subject'], FILTER_SANITIZE_STRING);
 $message = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
 
-$mailheader = "From:" . $name . "<" . $email . ">\r\n";
+// Validate required fields
+if (empty($name) || empty($email) || empty($subject) || empty($message)) {
+    header("Location: https://javatechrunner.online/index.html?status=error&message=All fields are required", true, 303);
+    exit;
+}
 
-//$recipient = "prashu.raju@gmail.com, mail2activeshail@gmail.com, chan.shek2005@gmail.com";
+// Prepare email
 $recipient = "mail2activeshail@gmail.com";
+$mailheader = "From: $name <$email>\r\n";
+$mailheader .= "Reply-To: $email\r\n";
+$mailheader .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
-// mail($recipient, $subject, $message, $mailheader);
+$email_body = "Hi There,\n\n";
+$email_body .= "You have a new enquiry from e-actuell.com\n\n";
+$email_body .= "Client Name: $name\n";
+$email_body .= "Client Email: $email\n";
+$email_body .= "Subject: $subject\n";
+$email_body .= "Message: $message\n";
 
-// appending \r\n at the end of mailheaders for end
-
-//$message = "Hi There, You have new enquery from e-actuell.com  " . " CLIENT NAME " .  $name . " ||  " .  "CONTACT :" . $contact . "||  CLIENT'S MESSAGE:  " . $message;
-$message = "Hi There, You have new enquery from e-actuell.com  " . " CLIENT NAME " .  $name .    "||  CLIENT'S MESSAGE:  " . $message;
-
-$error = '';
-if (array_key_exists('contact', $_POST)) {
-    if (!preg_match('/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/', $_POST['contact'])) {
-        $error = 'Invalid Number!';
-    }
+// Send email
+if (mail($recipient, $subject, $email_body, $mailheader)) {
+    header("Location: https://javatechrunner.online/index.html?status=success&message=Thank you for contacting us. We will get back to you soon", true, 303);
+} else {
+    header("Location: https://javatechrunner.online/index.html?status=error&message=Failed to send message. Please try again later", true, 303);
 }
-if (mail($recipient, $subject, $message, $mailheader) or die("Error!")) {
-    $posted = true;
-    $result = true;
-    // $message = "Your contact information is received successfully.";
-    $type = "success";
-
-    if ($posted) {
-        if ($result) {
-            echo "<script type='text/javascript'>alert('Thank you for contacting Us. We will get back to you soon')</script>";
-            //header('Location: ../index.html');
-            header("Refresh:1; url=https://javatechrunner.online/index.html", true, 303);
-        } else {
-            echo "<script type='text/javascript'>alert('failed!')</script>";
-            //header('Location: ../index.html');
-            header("Refresh:1; url=https://javatechrunner.online/index.html", true, 303);
-            exit;
-        }
-    }
-}
-// mail($recipient, $subject, $message, $mailheader) or die("Error!");
+exit;
